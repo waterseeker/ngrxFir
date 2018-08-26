873d32272ad18dc7eabb2f2cfd912005cbb04ff9
Wayne Burris <waterseeker@Hotmail.com>
2018-08-26 12:28:28 -0500

    downgraded ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { StoreModue } from '@ngrx/store';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [
